8c2e4a9c4c1275ed07c84af0b090357c98b0c79f
Muhammad_Mamduh <muhammad.mma.oz@gmail.com>
2025-01-12 20:50:37 +0200

    test webhook_3 after using ngrok

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("I am merely a sample maven project to study devops");
         System.out.println("This code is from the JAVA APP itself");
         System.out.println("After using the reverse proxy webhook");
+        System.out.println("Push to test the webhook integration");
         System.out.println("********************************************************");
     }
 }
